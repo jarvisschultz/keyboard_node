befe8d481a5d3287862add52cfd90ea1dc46c897
Jarvis Schultz <jschultz@u.northwestern.edu>
2014-03-05 16:35:41 -0600

    Added the publishing of the latched topics, and a service handler for changing the state value and publishing it.

--- src/multi_keyboard.cpp
+++ src/multi_keyboard.cpp
@@ -8,13 +8,23 @@
 //---------------------------------------------------------------------------
 
 /*
-operating_condition
+operating_condition parameter
   0: Idle
   1: Calibrate
   2: Run
   3: Stop
   4: Emergency Stop
 */
+
+////////////////
+// PUBLISHERS //
+////////////////
+// operating_condition (puppeteer_msgs/OperatingCondition)
+
+//////////////
+// SERVICES //
+//////////////
+// operating_condition_change (provider) puppeteer_msgs/OperatingConditionChange
 
 
 //---------------------------------------------------------------------------
@@ -25,6 +35,8 @@ operating_condition
 #include "std_msgs/String.h"
 
 #include <puppeteer_msgs/RobotCommands.h>
+#include <puppeteer_msgs/OperatingCondition.h>
+#include <puppeteer_msgs/OperatingConditionChange.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,20 +62,27 @@ class KeyboardNode {
 private:
     ros::NodeHandle n_;
     ros::Publisher serial_pub;
+    ros::Publisher state_pub;
+    ros::ServiceServer op_change_server;
     ros::Timer timer, show;
+    puppeteer_msgs::OperatingCondition op_con_msg;
     int operating_condition;
   
 public:
     KeyboardNode() {
 	serial_pub = n_.advertise<puppeteer_msgs::RobotCommands>
 	    ("/keyboard_serial_commands", 1);
+	state_pub = n_.advertise<puppeteer_msgs::OperatingCondition>
+	    ("/operating_condition", 1, true);
+	op_change_server = n_.advertiseService
+	    ("/operating_condition_change", &KeyboardNode::op_change_server_cb, this);
 	timer = n_.createTimer(ros::Duration(0.02),
 			       &KeyboardNode::timerCallback, this);
 	show = n_.createTimer(ros::Duration(0.5),
 			      &KeyboardNode::update_screen, this);
     
 	ROS_INFO("Starting Keyboard Node...");
-	operating_condition = 0;
+	operating_condition = op_con_msg.IDLE;
     }
 
     void timerCallback(const ros::TimerEvent& e) {
@@ -83,7 +102,9 @@ public:
 	else {
 	    ROS_WARN("Cannot Find Parameter: operating_condition");
 	    ROS_INFO("Setting operating_condition to IDLE");
-	    ros::param::set("/operating_condition", 0);
+	    ros::param::set("/operating_condition", op_con_msg.IDLE);
+	    op_con_msg.state = op_con_msg.IDLE;
+	    state_pub.publish(op_con_msg);
 	    return;
 	}
     
@@ -102,7 +123,9 @@ public:
 		// can we move from our current state to this state?
 		if(operating_condition > 1) {
 		    ROS_INFO("Robots State: IDLE");
-		    ros::param::set("/operating_condition", 0);
+		    ros::param::set("/operating_condition", op_con_msg.IDLE);
+		    op_con_msg.state = op_con_msg.IDLE;
+		    state_pub.publish(op_con_msg);
 		}
 		else if(operating_condition == 0) {
 		    ROS_INFO("Already in IDLE state");
@@ -117,7 +140,9 @@ public:
 	    else if(c == 'C')	{
 		if(operating_condition < 1) {
 		    ROS_INFO("Robots State Change: CALIBRATE");
-		    ros::param::set("/operating_condition", 1);
+		    ros::param::set("/operating_condition", op_con_msg.CALIBRATE);
+		    op_con_msg.state = op_con_msg.CALIBRATE;
+		    state_pub.publish(op_con_msg);
 		}
 		else if(operating_condition == 1) {
 		    ROS_INFO("Already in CALIBRATE state");
@@ -131,23 +156,9 @@ public:
 	    else if(c == 'P')	{
 		if(operating_condition < 2) {
 		    ROS_INFO("Robots State Change: RUN");
-		    ros::param::set("/operating_condition", 2);
-		    // for current sensing system, we automatically need to send
-		    // the start command once we enter run state:
-		    if(ros::param::has("/robot_index")) {
-			ros::param::get("/robot_index", robot_index);
-		    }
-		    else {
-			ROS_WARN("Cannot Find Parameter: robot_index");
-			ROS_INFO("Setting robot_index to 0");
-			ros::param::set("/robot_index", 0);
-		    }
-		    ROS_INFO("Sending Start String");
-		    puppeteer_msgs::RobotCommands srv;
-		    srv.robot_index = robot_index;
-		    srv.type = (uint8_t) 'm';
-		    srv.div = 0;
-		    serial_pub.publish(srv);
+		    ros::param::set("/operating_condition", op_con_msg.RUN);
+		    op_con_msg.state = op_con_msg.RUN;
+		    state_pub.publish(op_con_msg);
 		}
 		else if(operating_condition == 2) {
 		    ROS_INFO("Already in RUN state");
@@ -185,26 +196,6 @@ public:
 			     "current state: %i", operating_condition);
 		}
 	    }
-	    // did we request the measured current trajectory?
-	    else if(c == 'Z') {
-		ROS_INFO("Requested current trajectory");
-		
-		// check to see if robot index parameter exists.
-		if(ros::param::has("/robot_index")) {
-		    ros::param::get("/robot_index", robot_index);
-		}
-		else {
-		    ROS_WARN("Cannot Find Parameter: robot_index");
-		    ROS_INFO("Setting robot_index to 0");
-		    ros::param::set("/robot_index", 0);
-		}
-		// define start string
-		puppeteer_msgs::RobotCommands srv;
-		srv.robot_index = robot_index;
-		srv.type = (uint8_t) 'z';
-		srv.div = 0;
-		serial_pub.publish(srv);
-	    }
 
 	    else if (isdigit(c))
 	    {
@@ -213,21 +204,20 @@ public:
 		ROS_INFO("Robot index is now %d",robot_index);
 	    }
 
-      
 	    // did we enter a stop command?
 	    else
 	    {
 		if(operating_condition < 3)
 		{
 		    emergency_flag = false;
-		    ros::param::set("/operating_condition", 4);
+		    ros::param::set("/operating_condition", op_con_msg.STOP);
+		    op_con_msg.state = op_con_msg.STOP;
+		    state_pub.publish(op_con_msg);
 		    ROS_INFO("Robots State Change: STOP"); 
 		}
 		else if(operating_condition == 3 || operating_condition == 4)
 		    ROS_INFO("Already in STOP or EMERGENCY STOP state");
-	
 		else
-		
 		    ROS_INFO("Cannot enter STOP state from current "
 			     "state: %i", operating_condition);
 	    }
@@ -236,6 +226,25 @@ public:
 	return;
     }
 
+    bool
+    op_change_server_cb(puppeteer_msgs::OperatingConditionChange::Request &req,
+			puppeteer_msgs::OperatingConditionChange::Response &resp)
+	{
+	    uint8_t val = req.state.state;
+	    if (val > op_con_msg.EMERGENCY)
+	    {
+		resp.error = true;
+		return false;
+	    }
+	    else
+		resp.error = false;
+	    operating_condition = val;
+	    ros::param::set("/operating_condition", val);
+	    op_con_msg.state = val;
+	    state_pub.publish(op_con_msg);
+	    return true;
+	}
+    
     void update_screen(const ros::TimerEvent& e)
 	{
 	    static int last = 0;
@@ -247,15 +256,28 @@ public:
 		{
 		case 0:
 		    ROS_INFO("Operating condition: IDLE");
+		    op_con_msg.state = op_con_msg.IDLE;
+		    state_pub.publish(op_con_msg);
 		    break;
 		case 1:
 		    ROS_INFO("Operating condition: CALIBRATE");
+		    op_con_msg.state = op_con_msg.CALIBRATE;
+		    state_pub.publish(op_con_msg);
 		    break;
 		case 2:
 		    ROS_INFO("Operating condition: RUN");
+		    op_con_msg.state = op_con_msg.RUN;
+		    state_pub.publish(op_con_msg);
 		    break;
 		case 3:
-		    ROS_INFO("Operating condition: IDLE");
+		    ROS_INFO("Operating condition: STOP");
+		    op_con_msg.state = op_con_msg.STOP;
+		    state_pub.publish(op_con_msg);
+		    break;
+		case 4:
+		    ROS_INFO("Operating condition: EMERGENCY");
+		    op_con_msg.state = op_con_msg.EMERGENCY;
+		    state_pub.publish(op_con_msg);
 		    break;
 		}
 	    }
